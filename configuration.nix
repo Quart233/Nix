50cb853497066fe4809178e4d1dc212954acfe27
kuaizi <qhethe525@gmail.com>
2025-08-04 16:46:35 +0800

    feat(i18n):  Chinese input method support.

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,25 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
+  # Input Method
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-gtk
+      fcitx5-configtool
+    ];
+  };
+
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.windowManager.dwm.enable = true;
