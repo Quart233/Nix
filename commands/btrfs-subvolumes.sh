f5af6d0fa0c16513c34c8b28dd732e385f05d016
kuaizi <qhethe525@gmail.com>
2025-10-21 21:16:39 +0800

    feat(commands): Mount points creation.

--- commands/btrfs-subvolumes.sh
+++ commands/btrfs-subvolumes.sh
@@ -8,4 +8,24 @@ btrfs su cr /mnt/@snapshots
 mount -o noatime,subvol=@ /dev/nvme0n1p3 /mnt
 mount -o noatime,subvol=@nix,compression=ZSTD /dev/nvme0n1p3 /mnt/nix
 mount -o noatime,subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o noatime,subvol=@snapshots /dev/nvme0n1p3 /mnt/snapshots
+mount -o noatime,subvol=@snapshots /dev/nvme0n1p3 /mnt/snapshots# Create subvolumes
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@nix
+btrfs su cr /mnt/@nix-snapshots
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@home-snapshots
+
+# Mount
+umount /mnt && mount -o noatime,subvol=@ /dev/nvme0n1p3 /mnt || exit 1
+
+# Create Mount Points
+mkdir -p /mnt/nix
+mkdir -p /mnt/nix/.snapshots
+mkdir -p /mnt/home
+mkdir -p /mnt/home/.snapshots
+
+# Mount Subvolumes
+mount -o noatime,subvol=@nix,compression=ZSTD /dev/nvme0n1p3 /mnt/nix
+mount -o noatime,subvol=@nix-snapshots,compression=ZSTD /dev/nvme0n1p3 /mnt/nix/.snapshots
+mount -o noatime,subvol=@home,compression=ZSTD /dev/nvme0n1p3 /mnt/home
+mount -o noatime,subvol=@home-snapshots /dev/nvme0n1p3 /mnt/home/.snapshots
