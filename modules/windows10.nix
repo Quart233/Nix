6dc040e84dac39f76cc8a7d25faa0cdff554ba09
kuaizi <qhethe525@gmail.com>
2026-06-11 21:18:55 +0800

    feat(libvirt): Hostdev dGPU passthrough.

--- modules/windows10.nix
+++ modules/windows10.nix
@@ -4,40 +4,35 @@
   virtualisation.libvirt.connections."qemu:///system".domains =
   [
     {
-      definition = NixVirt.lib.domain.writeXML (NixVirt.lib.domain.templates.windows
-      {
-        name = "windows10-dgpu";
-        uuid = "cc7439ed-36af-4696-a6f2-1f0c4474d87e";
-        memory = { count = 16; unit = "GiB"; };
-        vcpu = { count = 8; };
-
-        # Add host device
-        hostdevs = [
-          {
-            type = "pci";
-            managed = true;
-            source = {
-              address = { domain = 0; bus = 3; slot = 0; function = 0; };
-            };
-          }
-        ];
-
-        # Add disk format override
-        disks = [{
-          device = "disk";
-          type = "file";
-          driver = { type = "raw"; };
-          source = { file = "/var/lib/libvirt/btrfs-pool/Penguin.raw"; };
-          target = { dev = "vda"; bus = "virtio"; };
-        }];
-
-        nvram_path = "/var/lib/libvirt/qemu/nvram/windows10-dgpu_VARS.fd";
-
-        features = {
-          hyperv = true;           # Improve Windows performance
-          kvmHidden = true;        # Hide KVM spec features
+      definition = NixVirt.lib.domain.writeXML (
+        let base = NixVirt.lib.domain.templates.windows {
+          name = "win10-dgpu";
+          uuid = "def734bb-e2ca-44ee-80f5-0ea0f2593aaa";
+          memory = { count = 8; unit = "GiB"; };
+          storage_vol = { pool = "btrfs-pool"; volume = "win10.qcow2"; };
+          backing_vol = /home/kuaizi/VM-Storage/Base.qcow2;
+          install_vol = /home/kuaizi/VM-Storage/SW_DVD9_WIN_ENT_LTSC_2021_64BIT_ChnSimp_MLF_X22-84402.iso;
+          bridge_name = "virbr0";
+          nvram_path = /home/kuaizi/VM-Storage/win10.nvram;
+          virtio_net = true;
+          virtio_drive = true;
+          install_virtio = true;
         };
-      });
+        in
+        base // {
+          devices = base.devices // {
+            hostdev = (base.devices.hostdev or []) ++ [
+              {
+                type = "pci";
+                managed = true;
+                source = {
+                  address = { domain = 0; bus = 3; slot = 0; function = 0; };
+                };
+              }
+            ];
+          };
+        }
+      );
     }
   ];
 }
