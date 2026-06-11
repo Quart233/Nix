f5954df08fb8731ea864df969f401b6d89b85ab4
kuaizi <qhethe525@gmail.com>
2026-06-11 11:53:38 +0800

    Revert "chore(flakes): NixVirt from path."
    This reverts commit 4d3b1e6a11c7de4e734073353d4a828cca3e97a3.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     # ---- NixVirt (the module that lets you write pools/volumes in Nix) ----
     NixVirt = {
-      url = "path:/etc/nixos/flakes/NixVirt";
+      url = "https://flakehub.com/f/AshleyYakeley/NixVirt/0.6.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
