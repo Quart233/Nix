c017cad7046d0d16d4451a1dcedcc19a18979d69
kuaizi <qhethe525@gmail.com>
2026-06-11 20:02:50 +0800

    chore(pkgs): Enable chromium.

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,7 @@
   time.timeZone = "Asia/Shanghai";
 
   programs.firefox.enable = true;
+  programs.chromium.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
